1a8edce7c342a8ad6ccf2e8a8d661cf673f5d3a9
Avuong <avuong23@student.cccd.edu>
2022-11-03 13:47:35 -0700

    Update main.js and RecipeCard.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,14 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  var recipeArray = [];
+  if(localStorage.getItem('recipes') !== null)
+  {
+    recipeArray = JSON.parse(localStorage.getItem('recipes'));
+    //console.log(recipeArray);
+  }
+
+  return recipeArray;
 }
 
 /**
@@ -35,10 +43,21 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const main = document.querySelector('main');
+  
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  var recipeArray = getRecipesFromStorage();
+
+  for(let i = 0; i < recipes.length; i++)
+  {
+    const recipe = document.createElement('recipe-card');
+    //console.log(recipes[i]);
+    recipe.data = recipes[i];
+    main.append(recipe);
+  }
 }
 
 /**
@@ -51,6 +70,8 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+
+  
 }
 
 /**
